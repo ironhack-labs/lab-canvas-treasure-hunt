8f8ce1b4453d6d3e020e6287d369fea7c5c7d4cd
Joao Evangelho <joaoevangelho@MacBook-Pro-de-Joao-3.local>
2019-10-30 18:54:40 +0000

    exercise done

--- starter-code/main.js
+++ starter-code/main.js
@@ -40,11 +40,6 @@ class character {
 }
 
 const player = new character(0, 0);
-console.log(player);
-player.moveDown();
-player.moveDown();
-player.moveRight();
-console.log(player.col, player.row);
 
 
 
@@ -55,36 +50,71 @@ const drawPlayer = () => {
     image.addEventListener('load', () => {
         const imageWidth = image.width;
         const imageHeight = image.height;
-        context.drawImage(image, 0, 0, imageWidth, imageHeight);
+        context.drawImage(image, player.col * 50, player.row * 50, imageWidth, imageHeight);
     });
 }
 
+
 class Treasure {
-    setRandomPosition(col, row) {
-        this.row = Math.floor(Math.random() * 10);
-        this.col = Math.floor(Math.random() * 10);
+    constructor(col, row) {
+        this.col = col;
+        this.row = row;
     }
 
+    setRandomPosition() {
+        this.col = Math.floor(Math.random() * 10);
+        this.row = Math.floor(Math.random() * 10);
+    }
 }
 
-const treasure = new treasure(0, 0);
-console.log(treasure);
+
+const treasure = new Treasure(0, 0);
+treasure.setRandomPosition();
 console.log(treasure.col, treasure.row);
+
 
 const drawTreasure = () => {
     const IMAGE_URL = "./images/treasure.png";
     const image = new Image();
     image.src = IMAGE_URL;
     image.addEventListener('load', () => {
-        const imageWidth = image.width;
-        const imageHeight = image.height;
-        treasure.setRandomPosition()
-        context.drawImage(image, treasure.col * 50, treasure.row * 50, imageWidth / 5, imageHeight / 5);
+        const imageWidth = image.width / 5;
+        const imageHeight = image.height / 5;
+        context.drawImage(image, treasure.col * 50, treasure.row * 50, imageWidth, imageHeight);
     });
 }
 
+window.addEventListener('keydown', (event) => {
+    // Stop the default behavior (moving the screen to the left/up/right/down)
+    event.preventDefault();
+
+    // React based on the key pressed
+    switch (event.keyCode) {
+        case 37:
+            player.moveLeft();
+            console.log('left');
+            drawEverything();
+            break;
+        case 38:
+            player.moveUp();
+            console.log('up');
+            drawEverything();
+            break;
+        case 39:
+            player.moveRight();
+            console.log('right');
+            drawEverything();
+            break;
+        case 40:
+            player.moveDown();
+            console.log('down');
+            drawEverything();
+            break;
+    }
+});
 
 function drawEverything() {
+    context.clearRect(0, 0, 500, 500);
     drawGrid();
 
     drawPlayer();
@@ -95,24 +125,4 @@ function drawEverything() {
 drawEverything();
 
 /*
-window.addEventListener('keydown', (event) => {
-  // Stop the default behavior (moving the screen to the left/up/right/down)
-  event.preventDefault();
-
-  // React based on the key pressed
-  switch (position) {
-    case moveUp:
-      console.log('up');
-      break;
-    case moveLeft:
-      console.log('left');
-      break;
-    case moveRight:
-      console.log('right');
-      break;
-    case moveDown:
-      console.log('down');
-      break;
-  }
-};
-*/
+ */
