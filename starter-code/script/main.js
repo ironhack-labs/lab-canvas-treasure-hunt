0cfdb91e72a717e001fee21b8d1e27cb28dd9dc3
Sam Vargas <sam.vargas@gmx.com>
2019-10-30 19:37:20 +0000

    bonus - player direction. Code refactored.

--- starter-code/script/main.js
+++ starter-code/script/main.js
@@ -1,8 +1,8 @@
-// Iteration 2
-class Character {
+class Player {
   constructor(){
     this.col = 0;
     this.row = 0;
+    this.direction = 'down';
   }
 
   moveUp() {
@@ -36,23 +36,22 @@ class Treasure {
 
 
 // main.js
-const canvas = document.querySelector('canvas');
-const context = canvas.getContext('2d');
+const $canvas = document.querySelector('canvas');
+const context = $canvas.getContext('2d');
 
-// I didn't see these at first...
-// const width = canvas.width;
-// const height = canvas.height;
-
-const WIDTH_OF_BOARD = canvas.width; 
+const WIDTH_OF_BOARD = $canvas.width; 
 const NUM_OF_COLS_AND_ROWS = 10;
 const WIDTH_OF_COLS = WIDTH_OF_BOARD / NUM_OF_COLS_AND_ROWS;
 
-let player = new Character;
+// Let's make a player
+let player = new Player;
 
+// Let's make some treasure!
 let treasure = new Treasure;
 treasure.setRandomPosition();
 
-// Iteration 1
+
+// Iteration 1 - Draw the board
 function drawGrid() {
 
   // draw vertical lines
@@ -74,39 +73,49 @@ function drawGrid() {
   }
 }
 
-function drawPlayer() {
-  const image = new Image();
-  image.src = 'images/character-down.png';
-
+function drawPlayer(player) {
+  let image;
+  // Switch doesn't seem to work but if does...
+  if (player.direction === 'up') {
+    const playerImgUp = new Image();
+    playerImgUp.src = 'images/character-up.png';
+    image = playerImgUp;
+  } else if (player.direction === 'right') {
+    const playerImgRight = new Image();
+    playerImgRight.src = 'images/character-right.png';
+    image = playerImgRight;
+  } else if (player.direction === 'down') {
+    const playerImgDown = new Image();
+    playerImgDown.src = 'images/character-down.png';
+    image = playerImgDown;
+  } else if (player.direction === 'left') {
+    const playerImgLeft = new Image();
+    playerImgLeft.src = 'images/character-left.png';
+    image = playerImgLeft;
+  }
+  
   image.addEventListener('load', () => {
-      context.drawImage(image, player.col * 50, player.row  * 50, 48, 48);
+    context.drawImage(image, player.col * 50, player.row  * 50, 48, 48);
   });
-  console.log(`Player row: ${player.row}, Player col: ${player.col}`);
-}
+} 
 
 function drawTreasure() {
-  let col = treasure.col;
-  let row = treasure.row;
-  // console.log(`Column: ${col}, Row: ${row}`);
-  // context.font = '32px Helvetica, sans-serif';
-  // context.fillText('T', (col * 50) + 20, (row * 50) + 50); 
-
   const image = new Image();
   image.src = 'images/treasure.png';
   image.addEventListener('load', () => {
-      context.drawImage(image, col * 50, row * 50, 48, 48);
+      context.drawImage(image, treasure.col * 50, treasure.row * 50, 48, 48);
   });
 }
 
 function drawEverything() {
   drawGrid();
-  drawPlayer();
+  drawPlayer(player);
   drawTreasure();
 }
 
-drawEverything();
-
 function clearPosition(col, row) {
+  // size needs to be smaller than player & treasure images 
+  // & centered to stop removing the board lines
   context.clearRect((col*50)+1, (row*50)+1, 46, 46);
 }
 
@@ -117,30 +126,38 @@ window.addEventListener('keydown', (event) => {
   // React based on the key pressed
   switch (event.keyCode) {
     case 37:
-      console.log('left');
+      // console.log('left');
       clearPosition(player.col, player.row);
       player.moveLeft();
+      player.direction = 'left';
       break;
     case 38:
-      console.log('up');
+      // console.log('up');
       clearPosition(player.col, player.row);
       player.moveUp();
+      player.direction = 'up';
       break;
     case 39:
-      console.log('right');
+      // console.log('right');
       clearPosition(player.col, player.row);
       player.moveRight();
+      player.direction = 'right';
       break;
     case 40:
-      console.log('down');
+      // console.log('down');
       clearPosition(player.col, player.row);
       player.moveDown();
+      player.direction = 'down';
       break;
   }
-  // Set new treasure position if player reaches it 
+  // Make some new treasure if player reaches it 
   if (player.col == treasure.col && player.row == treasure.row) {
+    clearPosition(treasure.col, treasure.row);
     treasure.setRandomPosition();
     drawTreasure();
   }
-  drawPlayer();
+  drawPlayer(player);
 });
+
+
+drawEverything();
