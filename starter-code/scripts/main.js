84049fe8f35b5e4d560efd1f127edf50aef43621
Alntjan <oliveiracatarino88@gmail.com>
2019-10-30 14:47:44 +0000

    Iteration 3

--- starter-code/scripts/main.js
+++ starter-code/scripts/main.js
@@ -4,14 +4,6 @@ const context = canvas.getContext('2d');
 const width = canvas.width;
 const height = canvas.height;
 
-// Iteration 1
-function drawGrid() {
-  for (i=0;i<10;i++){
-    context.strokeRect(i*width/10,0,width/10,height);
-    context.strokeRect(0,i*width/10,width,height/10);
-  }
-}
-
 class Character {
     constructor(col, row){
         this.col = col;
@@ -31,9 +23,37 @@ class Character {
     }
 }
 
+
+const player = new Character(0, 0);
+console.log(player.col, player.row);
+
+
+// Iteration 1
+function drawGrid() {
+  for (i=0;i<10;i++){
+    context.strokeRect(i*width/10,0,width/10,height);
+    context.strokeRect(0,i*width/10,width,height/10);
+  }
+}
+
+
+function drawPlayer(){
+    const IMAGE_URL = './images/character-down.png';
+    const image = new Image();
+    image.src = IMAGE_URL;
+    console.dir(image);
+    image.addEventListener('load', () => {
+        const imageHeight = image.height;
+        console.log("Height", imageHeight);
+        const imageWidth = image.width;
+        console.log("Width", imageWidth);
+        context.drawImage(image, player.col, player.row, 50, 50);
+    });
+}
+
 function drawEverything() {
-  drawGrid();
-  // drawPlayer()
+    drawGrid();
+    drawPlayer();
   // drawTreasure()
 }
 
