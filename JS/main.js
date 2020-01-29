c582400d1b9720c9d3cba2ea7be2af5b9eb53d98
FilipeAVNunes <filipenunes407@hotmail.com>
2020-01-29 16:31:32 +0000

    a little bit more

--- JS/main.js
+++ JS/main.js
@@ -3,11 +3,48 @@ const canvas = document.querySelector('canvas');
 const context = canvas.getContext('2d');
 const width = canvas.width;
 const height = canvas.height;
-let xvalue = 0;
-let yvalue = 0;
 // Iteration 1
+
+// CLASSES
+class Character {
+  constructor(col, row) {
+    this.col = col;
+    this.row = row;
+  }
+
+  moveUp(up) {
+    this.row -= 50;
+  }
+
+  moveRight(right) {
+    this.col += 50;
+  }
+
+  moveDown(down) {
+    this.row += 50;
+  }
+
+  moveLeft(left) {
+    this.col -= 50;
+  }
+}
+
+class Treasure {
+  constructor(col, row) {
+    this.col = col;
+    this.row = row;
+  }
+
+  setRandomPosition() {}
+}
+
+const player = new Character(0, 0);
+
 function drawGrid() {
+  let xvalue = 0;
+  let yvalue = 0;
   // TODO: write the code of the function
+  context.strokeStyle = 'black';
   for (let i = 0; i < 10; i++) {
     for (let j = 0; j < 10; j++) {
       context.beginPath();
@@ -26,7 +63,7 @@ function drawPlayer() {
   characterImage.src = characterImageSrc;
 
   characterImage.addEventListener('load', () => {
-    context.drawImage(characterImage, 0, 0);
+    context.drawImage(characterImage, player.col, player.row);
   });
 }
 
@@ -41,36 +78,39 @@ function drawTreasure() {
 }
 
 function drawEverything() {
+  context.clearRect(0, 0, 500, 500);
   drawGrid();
   drawPlayer();
   drawTreasure();
 }
 
+window.addEventListener('keydown', event => {
+  // Stop the default behavior (moving the screen to the left/up/right/down)
+  event.preventDefault();
+
+  // React based on the key pressed
+  switch (event.keyCode) {
+    case 37:
+      player.moveLeft();
+      console.log('left');
+      drawEverything();
+      break;
+    case 38:
+      player.moveUp();
+      console.log('up');
+      drawEverything();
+      break;
+    case 39:
+      player.moveRight();
+      console.log('right');
+      drawEverything();
+      break;
+    case 40:
+      player.moveDown();
+      console.log('down');
+      drawEverything();
+      break;
+  }
+});
+
 drawEverything();
-
-// iteration 2
-
-class Character {
-  constructor(col, row) {
-    this.col = col;
-    this.row = row;
-  }
-
-  moveUp(up) {
-    return (this.row -= 1);
-  }
-
-  moveRight(right) {
-    return (this.col += 1);
-  }
-
-  moveDown(down) {
-    return (this.row += 1);
-  }
-
-  moveLeft(left) {
-    return (this.col -= 1);
-  }
-}
-
-const player = new Character(0, 0);
